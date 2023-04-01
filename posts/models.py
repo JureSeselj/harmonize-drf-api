45e9add8ef7633b0b545895e346d26153bfa9f45
JureSeselj <jure.seselj@gmail.com>
2023-04-01 10:52:37 +0000

    Fix typo in the Post model and made migrations

--- posts/models.py
+++ posts/models.py
@@ -32,7 +32,7 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-created_on']
 
     def __str__(self):
         return f'{self.id} {self.title}'
