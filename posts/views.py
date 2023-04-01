07314fbf4aa9d755482dd06749bf806313d53684
JureSeselj <jure.seselj@gmail.com>
2023-04-01 11:21:35 +0000

    Add get method on PostDetail view

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
+from harmonize_drf_api.premissions import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -32,3 +34,35 @@ class PostList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    """
+    A class view for post details
+    to enable users to retrieve, update and delete the post
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        """
+        Fetch the posts by id
+        Handle the case a post does not exist
+        """
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """
+        Retrieve a post by id
+        """
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post,
+            context={'request': request}
+            )
+        return Response(serializer.data)
