4d59658c6d76e127407b119408f5b7637d0e3765
JureSeselj <jure.seselj@gmail.com>
2023-04-01 11:16:05 +0000

    Add post method to create Posts

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post
@@ -9,6 +9,11 @@ class PostList(APIView):
     """
     A class view for the PostList
     """
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(
@@ -17,3 +22,13 @@ class PostList(APIView):
             context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data,
+            context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
