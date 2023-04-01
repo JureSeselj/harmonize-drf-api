01b3070280cd827fe349dc8705c6adae03afaa36
JureSeselj <jure.seselj@gmail.com>
2023-04-01 11:06:37 +0000

    Add Posts get method to list all posts and url paths

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    """
+    A class view for the PostList
+    """
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
