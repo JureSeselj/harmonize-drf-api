1b31ab2fb36083b21dda39b68067caf8d5587ecf
JureSeselj <jure.seselj@gmail.com>
2023-04-01 18:56:16 +0000

    Add JWT_AUTH_SAMESITE to None in settings.py

--- harmonize_drf_api/settings.py
+++ harmonize_drf_api/settings.py
@@ -50,6 +50,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'harmonize_drf_api.serializers.CurrentUserSerializer'
