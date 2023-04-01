8b8c71c1c40fbb350f4a2db47b91c5a79c3f6b27
JureSeselj <jure.seselj@gmail.com>
2023-04-01 10:31:43 +0000

    Add permission for owners only to edit their profile

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from harmonize_drf_api.premissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
@@ -21,6 +22,7 @@ class ProfileDetail(APIView):
     A class for the Profile Details
     """
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         """
@@ -28,6 +30,7 @@ class ProfileDetail(APIView):
         """
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
