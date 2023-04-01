4b5aa5c594a7255fc8160752b81952fe1af561ff
JureSeselj <jure.seselj@gmail.com>
2023-04-01 18:17:52 +0000

    Add automated testing of ProfileDetailView

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileDetailViewTests(APITestCase):
+    def setUp(self):
+        """
+        Creates two users instances
+        An associated profile instance is automatically created using a signal
+        """
+        User.objects.create_user(username='jure', password='password')
+        User.objects.create_user(username='rosa', password='password')
+
+    def test_user_can_view_existing_profile(self):
+        """
+        Test if possible to view existing profile (which has a valid ID)
+        """
+        self.client.login(username='jure', password='password')
+        response = self.client.get('/profiles/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_view_non_existing_profile(self):
+        """
+        Test if possible to view a profile which doesn't exist
+        (no valid ID)
+        """
+        self.client.login(username='jure', password='password')
+        response = self.client.get('/profiles/9999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_their_own_profile(self):
+        """
+        Test if user can update a profile they own
+        """
+        self.client.login(username='jure', password='password')
+        response = self.client.put('/profiles/1/',
+                                   {'description': 'hello world'})
+        profile = Profile.objects.filter(pk=1).first()
+        self.assertEqual(profile.description, 'hello world')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_update_other_profiles(self):
+        """
+        Test if user cannot update other users' profiles
+        """
+        self.client.login(username='jure', password='password')
+        response = self.client.put('/profiles/2/', {'description': 'hi there'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_not_logged_in_user_can_update_their_own_profile(self):
+        """
+        Test if user can update their profile when not logged in
+        """
+        response = self.client.put('/profiles/1/',
+                                   {'description': 'hello world'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_their_profile(self):
+        """
+        Test if user can delete their profile
+        """
+        self.client.login(username='jure', password='password')
+        response = self.client.delete('/profiles/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
