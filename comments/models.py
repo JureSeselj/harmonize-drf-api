9fff1eabefb3a6f01ddabddaaef2b550a882d6fb
JureSeselj <jure.seselj@gmail.com>
2023-04-01 11:42:30 +0000

    Create Comments app and added serializer for Comments model

---
+++ comments/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+
+class Comment(models.Model):
+    """
+    A class for the comment model
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    comment_content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.comment_content
