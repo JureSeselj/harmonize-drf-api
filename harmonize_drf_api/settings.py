dc76c891205569bab12f4872502e40f0682ba3a5
JureSeselj <jure.seselj@gmail.com>
2023-04-02 10:53:54 +0000

    Change ALLOWED_HOSTS in settings.py file

--- harmonize_drf_api/settings.py
+++ harmonize_drf_api/settings.py
@@ -67,7 +67,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'harmonize-drf-api.herokuapp.com',
+    os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
 
